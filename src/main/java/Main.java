5f21123092f74ac42adafc97551a671192972fd5
Cleverson102 <169068234+Cleverson102@users.noreply.github.com>
2024-05-13 14:37:50 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("╭──────༺༻──────╮");
+    System.out.println("|*Melanie Martinez|");
+    System.out.println("╰──────༺༻──────╯");
+
+   
+    System.out.println("");
+    
+    System.out.println("Álbum: Portals");
+    System.out.println("Data de Lançamento:21 de Março de 2023");
+  }
+}
